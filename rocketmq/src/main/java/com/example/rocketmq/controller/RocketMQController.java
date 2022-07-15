0244ca9f7bcca405a2c4fc6040b2afc20f6bd976
wuhao <wuhao.cosmo@haier.com>
2022-07-15 14:30:19 +0800

    修改

--- rocketmq/src/main/java/com/example/rocketmq/controller/RocketMQController.java
+++ rocketmq/src/main/java/com/example/rocketmq/controller/RocketMQController.java
@@ -1,6 +1,8 @@
 package com.example.rocketmq.controller;
 
 import com.example.rocketmq.service.MQProducerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rocketmq")
+@Api(tags = "rocketmq")
 public class RocketMQController {
 
     @Autowired
     private MQProducerService mqProducerService;
 
     @GetMapping("/send")
+    @ApiOperation(value = "发送信息", notes = "发送信息")
     public void send() {
         mqProducerService.send("user");
     }
     
     @GetMapping("/sendTag")
+    @ApiOperation(value = "发送tag信息", notes = "发送tag信息")
     public SendResult sendTag() {
         return mqProducerService.sendTagMsg("带有tag的字符消息");
     }
